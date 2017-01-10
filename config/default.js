a9f80c17da4afab8afe3b75b03d5628eabc556ba
ubandma <manoj.bandara@pearson.com>
2017-01-10 13:12:00 +0530

    Added redis properties

--- config/default.js
+++ config/default.js
@@ -14,6 +14,9 @@ module.exports = {
     logDir: '../log/',
     logAllowWebAccess: true,
 
+    redisHost: 'redis',
+    redisPort: 6379,
+
 
     addressLookupUri:'http://ws.postcoder.com/pcw/PCW45-12345-12345-1234X/address/uk/{address-fragment}?format=json',
     streetLookupUri:'http://ws.postcoder.com/pcw/PCW45-12345-12345-1234X/street/uk/{address-fragment}?format=json',
